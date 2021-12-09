30cbcfa91f9647b3b00b25879a0917859a5b11c2
IOpahI <doriangudenus@gmail.com>
2021-12-09 17:32:18 +0000

    updates

--- Audio Visualiser/Assets/scripts/terrainfollow.cs
+++ Audio Visualiser/Assets/scripts/terrainfollow.cs
@@ -7,7 +7,7 @@ public class terrainfollow : MonoBehaviour
 
     void Update()
     {
-        Vector3 myPos = transform.position; //grabbing the curent vector3 of the camera
+        Vector3 myPos = transform.position;                  //grabbing the curent vector3 of the camera
 
         myPos.y = Terrain.activeTerrain.SampleHeight(myPos); // grabbing the height of the terrain under the camera
         myPos.y += 4;                                        // adding onto the terrain height
